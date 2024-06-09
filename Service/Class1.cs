f2cbbc0553606ff45976366fcee45ee22c5afa0b
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-06-09 20:56:56 +0700

    up

---
+++ Service/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace Service
+{
+    using System.Data.SQLite;
+
+
+
+    public class Class1
+    {
+        using (var connection = new SQLiteConnection("C:\\mydatabase.db"))
+{
+    // Tạo database (nếu chưa tồn tại)
+    connection.CreateIfNotExists();
+
+    // Tạo table (nếu chưa tồn tại)
+    string sql = "CREATE TABLE MyTable (Id INTEGER PRIMARY KEY, Name TEXT, Price REAL)";
+    using (var command = new SQLiteCommand(sql, connection))
+    {
+        command.ExecuteNonQuery();
+    }
+}
+    }
+}
