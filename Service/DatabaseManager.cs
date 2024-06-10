703e3394fa3deeca827ea747fb6dd9a8d6cf9f00
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-06-10 16:01:25 +0700

    up

--- Service/DatabaseManager.cs
+++ Service/DatabaseManager.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.Storage;
 using System.IO;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 namespace Service
 {
     public class DatabaseManager
     {
+        //dotnet tool install --global dotnet-ef --version 6.0.423
+        //dotnet ef migrations add InitialCreate
+        //dotnet ef database update
     }
 }
